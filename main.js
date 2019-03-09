236172dde1d8934295dc6e6e5a63fd79d0dca201
rustylake <43626897+rustylake@users.noreply.github.com>
2019-03-09 14:15:02 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,20 +3,21 @@ var data={
 "项":"内容"，//可添加
 };
 
-
-var change = function (i,data) {
-    var fro=document.getElementsByTagName("input")[i];//对应输入框
-    var spa=document.getElementsByTagName("span")[i-1].innerHTML.replace("\"","");
-    var kk=document.createAttribute("value");//值
-    if(data[spa]){
-    kk.nodeValue=data[spa];
-    fro.attributes.setNamedItem(kk);
-    console.log(fro,spa,kk);}
+var change = function (i, data) {
+    var fro = document.getElementsByTagName("input")[i];//对应输入框
+    if (fro.type === "text") tex(fro, i - 1);//文本框
+    if (fro.type==="radio") fro.checked=true;//填写只有一个的单选
 };
 
-for (var i=1;i<10;i++){
-    change(i,data)
+var tex = function (fro, i) {
+    var spa = document.getElementsByTagName("span")[i].innerHTML.replace("\"", "");
+    var kk = document.createAttribute("value");//值
+    kk.nodeValue = data[spa];
+    fro.attributes.setNamedItem(kk);
 }
+
+for (var i = 1; i < 15; i++) {
+    change(i, data)}
 
 
 
